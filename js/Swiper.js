e38f49194774bc0a18f8db4a0899d50a39fe0819
VyacheslavVoronov <143994690+VyacheslavVoronov@users.noreply.github.com>
2025-05-10 00:43:14 +0300

    feat: some code changes for slider

--- js/Swiper.js
+++ js/Swiper.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.testimonial-swiper', {
-        loop: true,
-        autoplay: false,
-        allowTouchMove: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        slidesPerView: 1,
-        spaceBetween: 25,
-        watchOverflow: false,
-    });
+  const swiper = new Swiper('.comments-slider', {
+      slidesPerView: 1,
+      spaceBetween: 36,
+      loop: false,
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+  });
 });
